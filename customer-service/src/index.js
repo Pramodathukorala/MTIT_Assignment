2458b031c747beb21d5a5b9f95170f5d6e3c25a8
Pramodathukorala <155905078+Pramodathukorala@users.noreply.github.com>
2026-03-31 10:26:30 +0530

    Customer service

---
+++ customer-service/src/index.js
@@ -0,0 +1,13 @@
+require("dotenv").config();
+const app = require("./app");
+const connectDB = require("./config/db");
+
+const PORT = process.env.PORT || 8082;
+
+// Connect to MongoDB and start server
+connectDB().then(() => {
+  app.listen(PORT, () => {
+    console.log(`Customer Service running on port ${PORT}`);
+    console.log(`Swagger docs: http://localhost:${PORT}/api-docs`);
+  });
+});
